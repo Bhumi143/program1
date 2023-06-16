4e9868722e77153fc7e6d7c04fb76a58ff4343e8
Lenovo <Lenovo@SHREE-RAM143>
2023-06-16 21:52:09 +0530

    main program

--- program1/src/Program.java
+++ program1/src/Program.java
@@ -4,6 +4,7 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("jay shree ram");
+		System.out.println("radhe radhe");
 	}
 
 }
